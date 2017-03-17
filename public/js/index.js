5990dc1b4a6e3bd9eb69631a9249442a457135b1
benoitgoupilleau <benoit.goupilleau@gmail.com>
2017-03-17 10:36:15 +0100

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect', function(){
   console.log('Connected to server');
 
-  socket.emit('createMessage', {
-    from: 'Benoit',
-    text: 'Message created'
-  });
 });
 
 socket.on('disconnect', function(){
