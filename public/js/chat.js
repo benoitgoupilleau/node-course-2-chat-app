06c602baaa1014e354f06f44cc1079713f4db2be
benoitgoupilleau <benoit.goupilleau@gmail.com>
2017-03-20 09:57:21 +0100

    Add join page

--- public/js/chat.js
+++ public/js/chat.js
@@ -26,6 +26,7 @@ socket.on('disconnect', function(){
 });
 
 socket.on('newMessage', function(message){
+  if(message.text.length >0){
   var formattedTime = moment(message.createdAt).format('h:mm a');
   var template = jQuery("#message-template").html();
   var html = Mustache.render(template, {
@@ -36,6 +37,7 @@ socket.on('newMessage', function(message){
 
   jQuery("#messages").append(html);
   scrollToBottom();
+}
 });
 
 socket.on('newLocationMessage', function (message){
